b29309ac74cced5feef0ae9ec0f06796862ada4a
rishiporwal <64612539+rishiporwal@users.noreply.github.com>
2020-10-05 16:38:18 -0500

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,37 @@
+namespace spritePackSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite19 = image.ofBuffer(hex``);
+
+}
